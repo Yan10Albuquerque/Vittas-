3ec129579078007846acfbc6172b1deb05ccdec8
Yan <yanflusantos@gmail.com>
2026-04-26 17:39:52 -0300

    static

--- ciil/settings.py
+++ ciil/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in {"1", "true", "yes", "on"}
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["127.0.0.1", "vittas.onrender.com"]
