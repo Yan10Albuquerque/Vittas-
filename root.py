e98af3a7004f4979b44f64a745af4a19530ba251
Yan <yanflusantos@gmail.com>
2026-04-26 17:27:00 -0300

    root

---
+++ root.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Cria superuser automaticamente'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        if not User.objects.filter(username='root').exists():
+            User.objects.create_superuser(
+                username='root',
+                email='root@email.com',
+                password='Yan27112001@'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser criado!'))
+        else:
+            self.stdout.write('Superuser já existe.')
